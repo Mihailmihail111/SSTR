86d00c927501fb91e5ad078730979274c49e2224
wert_ua <microsof3213aa@gmail.com>
2020-08-07 19:20:41 +0300

    таблица

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap' }
     ]
   },
   /*
---
+++ pages/table_block/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <table_block/>
+</template>
+
+<script>
+import table_block from '~/components/table_block'
+    export default {
+        components: {
+            table_block,
+        }
+    }
+</script>
