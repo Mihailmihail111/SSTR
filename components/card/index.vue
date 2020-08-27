0df166cef2100003874bf912ade46695303d246e
wert_ua <microsof3213aa@gmail.com>
2020-08-27 20:50:07 +0300

    правки

--- components/card/index.vue
+++ components/card/index.vue
@@ -19,6 +19,6 @@
     .card_page{
         width: 100%;
         padding: 25px 0;
-        background: url(~@/assets/images/card_bg.jpg) no-repeat center 0;
+        background: url(~@/assets/images/card_bg.png) left 0;
     }
 </style>
